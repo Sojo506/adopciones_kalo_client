a4bae82c9a523bd2c458e101f25fe097db250b46
Sojo506 <fsojodev@gmail.com>
2026-03-15 23:41:08 -0600

    Configure auth axios client

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,16 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  withCredentials: true,
+});
+
+// Attach bearer token automatically if stored
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken");
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
