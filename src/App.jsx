cdc338731594b1e4f182d88d97deb41aeeb76c86
Sojo506 <fsojodev@gmail.com>
2026-03-15 23:41:08 -0600

    Wire app shell for auth flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import AppRouter from "./routes/AppRouter";
+
 function App() {
-  return <h1>Adopciones Kalö</h1>;
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <div className="d-flex flex-column min-vh-100">
+          <Header />
+          <main className="flex-grow-1">
+            <AppRouter />
+          </main>
+          <Footer />
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
